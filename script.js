940ec5d40c18c7a61b75c53e95fe39003a264982
Firdosh Jamadar <firdoshjamadar0@gmail.com>
2025-02-11 13:31:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 //complete this code
 class Rectangle {
-	constructor(width, height) {
-		if(width<0 || height<0){
-		  throw new Error("Width and height must be positive values.");
-			
-		}
+    constructor(width, height) {
+        if (width < 0 || height < 0) {
+            throw new Error("Width and height must be positive values.");
+        }
         this._width = width;
         this._height = height;
     }
-	get width() {
+
+    get width() {
         return this._width;
     }
-	get height() {
+
+    get height() {
         return this._height;
     }
-	getArea() {
+
+    getArea() {
         return this._width * this._height;
     }
 }
 
-class Square extends Animal {
-	constructor(side) {
-		if(side<0){
-			throw new error("give me positive value")
-		}
-        super(side, side); 
+class Square extends Rectangle {
+    constructor(side) {
+        if (side < 0) {
+            throw new Error("Side length must be a positive value.");
+        }
+        super(side, side); // Pass side as both width and height
     }
-    getPerimeter(){
-        return 4 * this._width;
+
+    getPerimeter() {
+        return 4 * this.width; // Use the getter method
     }
 }
+
+// Do not change the code below this line
+window.Rectangle = Rectangle;
+window.Square = Square;
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
